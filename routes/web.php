608ce1ec981ab8aed5a1da247864dc76f25b357a
rastinbw <rastinbw@gmail.com>
2021-04-17 17:20:01 +0430

    hello world

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/api', function () {
     return view('welcome');
 });
+
+Route::get('/api/hello', function (){
+    return '<h1>Hello World</h1>';
+});
