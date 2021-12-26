f4165499e9f6f0badafa3bd28bf85db7d7305cb4
Rastin <rastinbw@gmail.com>
2021-12-26 13:49:44 +0330

    app api update

--- app/Http/Controllers/API/Student/App/CoursesController.php
+++ app/Http/Controllers/API/Student/App/CoursesController.php
@@ -40,7 +40,7 @@ class CoursesController extends BaseController
     public function registerCourseInDevice(Request $request){
         $lk = $request->input('lk');
         $old_lk = $request->input('old_lk');
-        $deviceInfo = (object)$request->input('device_info');
+        $deviceInfo = $request->input('device_info');
         $tenant = Tenant::find(User::where('key', substr($lk, 0, 4))->first()->tenant_id);
 
         if(!$tenant) return $this->sendResponse(Constant::$USER_NOT_FOUND, null);
@@ -95,7 +95,7 @@ class CoursesController extends BaseController
 
             }elseif($d1 && !$d2){
 
-                if($d1->imei != $deviceInfo->imei){
+                if($d1->imei != (object)$deviceInfo->imei){
                     $licenseKey->device_two = $deviceInfo;
                     $licenseKey->save();
                 }
